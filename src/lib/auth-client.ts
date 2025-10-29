170987c4680a5a2acd174d8400197abd99d8ec46
Amrit Ghimire <73785840+amrit713@users.noreply.github.com>
2025-10-29 11:54:34 +0545

    auth refactor

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,18 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins"
-import type { auth } from "@/lib/auth";
-import { inferAdditionalFields } from "better-auth/client/plugins";
+
+
 
 export const authClient = createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL!,
     plugins: [
         adminClient(),
-        inferAdditionalFields({
-            user: {
-                status: {
-                    type: "string"
-                }
-            }
-        }),
+
     ]
 });
