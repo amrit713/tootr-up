728331a15afb3916cd1f0e5904309c79efce5eb7
Amrit Ghimire <73785840+amrit713@users.noreply.github.com>
2025-12-22 19:28:16 +0545

    refactor: leadStatus type add

--- src/features/leads/api/use-get-leads.ts
+++ src/features/leads/api/use-get-leads.ts
@@ -1,40 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
+import { LeadStatus } from "@/generated/prisma/";
 
 import { client } from "@/lib/rpc";
 
-
 interface useGetLeadsProps {
-    status?: string,
-    dueDate?: string,
-    search?: string,
-    assigneeId?: string
-
+    status?: string;
+    dueDate?: string;
+    search?: string;
+    assigneeId?: string;
 }
 
-
-export const useGetLeads = ({ status, dueDate, search, assigneeId }: useGetLeadsProps) => {
+export const useGetLeads = ({
+    status,
+    dueDate,
+    search,
+    assigneeId,
+}: useGetLeadsProps) => {
     const query = useQuery({
         queryKey: ["leads", status, dueDate, search, assigneeId],
         queryFn: async () => {
             const response = await client.api.leads.$get({
                 query: {
-                    status,
+                    status: status as LeadStatus,
                     dueDate,
-                    search, assigneeId
-                }
-            })
+                    search,
+                    assigneeId,
+                },
+            });
 
             if (!response.ok) {
                 throw new Error("failed to fetch leads");
             }
             const { data } = await response.json();
 
-
-
-
             return data;
-        }
-    })
+        },
+    });
 
-    return query
-}
+    return query;
+};
