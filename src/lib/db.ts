2aa1889bd65bc234f8b1349d5184e5cc8b542037
Amrit Ghimire <73785840+amrit713@users.noreply.github.com>
2026-01-12 14:30:08 +0545

    feat: change in prisma new version

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,25 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
+import "dotenv/config";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-import { PrismaClient } from "@/generated/prisma";
+const connectionString = process.env.DATABASE_URL;
 
+// 3. Create the native driver client (Pool)
+const pool = new Pool({ connectionString });
+
+// 4. Create the Prisma adapter instance
+const adapter = new PrismaPg(pool);
+
+// 5. Implement the Singleton Pattern
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
+
+export default db;
